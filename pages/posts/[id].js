05736c9638b1b590519b026d4000260aed90d586
sheva <stefan@codechem.com>
2020-06-07 20:44:59 +0200

    Final try to fix pipeline for vercel

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from 'components/Layout';
+import Layout from '../../components/Layout';
 import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
