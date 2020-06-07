a8b4cf8c92f4008eb1b426199983e7d5180e1c2b
sheva <stefan@codechem.com>
2020-06-07 20:10:21 +0200

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    plugins: [
+      'tailwindcss',
+      ...(process.env.NODE_ENV === 'production'
+        ? [
+            [
+              {
+                content: [
+                  './pages/**/*.{js,jsx,ts,tsx}',
+                  './components/**/*.{js,jsx,ts,tsx}'
+                ],
+                defaultExtractor: content =>
+                  content.match(/[\w-/:]+(?<!:)/g) || []
+              }
+            ]
+          ]
+        : [])
+    ]
+  }
